7f6461e63e364a30e1570e3f81854b015e49b99a
satgunsodhi <satgunsinghsodhi@gmail.com>
2024-05-31 13:10:52 +1000

    adding comments

--- Mad libs game/Mad Libs.py
+++ Mad libs game/Mad Libs.py
@@ -1,4 +1,6 @@
 #story bats are so cool
+
+#taking inputs
 color = input("enter Color: ")
 adjective1 = input("Enter Adjective: ")
 time = input("Enter Time: ")
@@ -9,4 +11,6 @@ food2 = input('Enter Food: ')
 verb = input('Enter Verb: ')
 noun = input('Enter Noun: ')
 number = input('Enter Number: ')
+
+#printing the story
 print("Bats are so cool! They are ", color,', ', adjective1, " animals which have wings. They like to fly around at ", time, " which makes some people scared of them. But bats are ", adjective2, ", and they don't want to hurt people. I have a pet bat that lives in ", place, ". I like to feed him ", food1, " and ", food2, ". He likes to ", verb, ". I am his favorite person, but he also likes ", noun, ". I want to convince my parents to get me ", number, " more bats.", sep='')
