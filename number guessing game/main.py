b00aec9a7571ea3cbba7a08950c50308f3400c26
satgunsodhi <satgunsinghsodhi@gmail.com>
2024-05-31 13:10:52 +1000

    adding comments

--- number guessing game/main.py
+++ number guessing game/main.py
@@ -1,6 +1,9 @@
 from random import randint
 
+# taking a random number from 1 to 100 from the random library
 value = randint(0,100)
+
+# game code
 while 1:
     i = int(input())
     if(i == value):
